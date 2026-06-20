84e9a1ff9799b39a8a567a117293ec6d7efe94dc
predixion286 <muntasir28@gmail.com>
2026-06-20 13:27:18 +0600

    Update scorers.js

--- api/scorers.js
+++ api/scorers.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
     const [scorersResult, cardsResult] = await Promise.allSettled([
       // Goals from football-data.org
       fetch(
-        "https://api.football-data.org/v4/competitions/WC/scorers?season=2026",
+        "https://api.football-data.org/v4/competitions/WC/scorers?season=2026&limit=1000",
         { headers: { "X-Auth-Token": "368a2224700a4ef9abca96eb9b8c4d9d" } }
       ).then(r => r.json()),
       // Cards from Supabase (manually entered via admin panel)
